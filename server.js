89fc27e73121a46def2e0efa0b9574eceede4f5f
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-25 12:20:21 -0700

    Modified home.html, survey.html, htmlRoutes.js and server.js. Deleted style.css

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var PORT = process.env.PORT;
-console.log(process.env.PORT);
+var PORT = 3000; 
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./app/routing/apiRoutes.js");
-require("./app/routing/htmlRoutes.js");
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on port " + PORT);
