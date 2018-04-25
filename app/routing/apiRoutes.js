20e5f3d2b2f38cdbb08655252194de15b7f60eb1
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-25 12:32:15 -0700

    Modified apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,23 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
+        var userScores = req.body.scores;
         var incompatibilityPoints = [];
         for (var i = 0; i < friends.length; i++) {
             incompatibilityPoints[i] = 0;
+            for (var j = 0; j < friends[i].scores.length; j++){
+                incompatibilityPoints[i] += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j]));
+            }
         }
+        var mostCompatibleFriend = friends[0];
+        var leastPoints = incompatibilityPoints[0];
+        for (var i = 1;  i < friends.length; i++) {
+            if (incompatibilityPoints[i] < leastIncompatibilityPoints) {
+                leastIncompatibilityPoints = incompatibilityPoints[i];
+                mostCompatibleFriend = friends[i];
+            }
+        }
+        res.json(mostCompatibleFriend);
+        friends.push(req.body);
     });
 }  
