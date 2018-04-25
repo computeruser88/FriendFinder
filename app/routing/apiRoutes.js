de88766e376d20787b99acb83eee072215a62ab0
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-25 15:24:54 -0700

    Modified friends.js, home.html, survey.html, and apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,3 @@
-var path = require("path");
 var friends = require("../data/friends.js");
 
 module.exports = function (app) {
@@ -9,9 +8,10 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         var userScores = req.body.scores;
         var incompatibilityPoints = [];
+        
         for (var i = 0; i < friends.length; i++) {
             incompatibilityPoints[i] = 0;
-            for (var j = 0; j < friends[i].scores.length; j++){
+            for (var j = 0; j < userScores.length; j++){
                 incompatibilityPoints[i] += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j]));
             }
         }
