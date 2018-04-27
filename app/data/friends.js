cd8e1479380270d8e00c0f2b72da43275942137e
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-27 16:48:08 -0700

    Modified friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
         name: "Jan",
-        photoUrl: "http://i274.photobucket.com/albums/jj267/createandmelt/selfie2.png",
+        photoUrl: "https://i274.photobucket.com/albums/jj267/createandmelt/selfie2.png",
         scores: ["5", "2", "5", "2", "1", "3", "5", "5", "5", "1"]
     }
 ]
