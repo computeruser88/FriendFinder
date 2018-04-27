826de219b3ff60c0e6ed195c107dc40105f2ea44
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-27 16:07:10 -0700

    Modified friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 module.exports = [
     {
-        name: "Jan Balangue",
+        name: "Jan",
         photoUrl: "http://i274.photobucket.com/albums/jj267/createandmelt/selfie2.png",
         scores: ["5", "2", "5", "2", "1", "3", "5", "5", "5", "1"]
     }
