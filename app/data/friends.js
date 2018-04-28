be879e72573e15ff9c321131422e83b4f71f8a0d
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-27 17:25:42 -0700

    Modified friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,5 +3,25 @@ module.exports = [
         name: "Jan",
         photoUrl: "https://i274.photobucket.com/albums/jj267/createandmelt/selfie2.png",
         scores: ["5", "2", "5", "2", "1", "3", "5", "5", "5", "1"]
+    },
+    {
+        name: "Natalie",
+        photoUrl: "https://unsplash.com/photos/B03Ny5mCEwI",
+        scores: ["4", "4", "3", "5", "5", "1", "5", "5", "5", "3"]
+    },
+    {
+        name: "Winona",
+        photoUrl: "https://unsplash.com/photos/pg_WCHWSdT8",
+        scores: ["3", "2", "1", "1", "1", "5", "3", "5", "5", "5"]
+    },
+    {
+        name: "Simone",
+        photoUrl: "https://unsplash.com/photos/cYbQEnBiLs4",
+        scores: ["1", "1", "5", "5", "3", "4", "5", "5", "3", "2"]
+    },
+    {
+        name: "Luke",
+        photoUrl: "https://unsplash.com/photos/u5T5b3lNYw8",
+        scores: ["5", "5", "1", "1", "1", "5", "1", "5", "5", "4"]
     }
 ]
